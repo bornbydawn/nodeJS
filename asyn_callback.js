c27ee56b119c43e68eaa995be5d80efd288454b9
Saksham Gupta <saksham.gupta@sap.com>
2018-04-21 20:12:52 +0530

    Formatted async_callback and before eventEmmiter

--- asyn_callback.js
+++ asyn_callback.js
@@ -1,39 +1,61 @@
 const fs = require('fs');
 
-const readFileAsArray = function(file,cb = () => {}) {
-return new Promise((resolve,reject) => {
- fs.readFile(file, function(err, data) {
+//cb = () => {} is added as a default cb funciton in case none is specified which will happen in the case if the function is used with promises
+const readFileAsArray = function (file, cb = () => { }) {
+	//for promise
+	return new Promise((resolve, reject) => {
+		fs.readFile(file, function (err, data) {
 
- if(err){
-	reject(err);
+			if (err) {
+				//for promise
+				reject(err);
+				
+				//for callback , this is not truly async, use process.nextTick for truly async
+				return cb(err);
+			}
 
-	return cb(err);
-	}
-
-const lines = data.toString().trim().split('\n');
-resolve(lines);
-cb(null,lines);
-});
-});
+			const lines = data.toString().trim().split('\n');
+			
+			//for promise
+			resolve(lines);
+			//for callback
+			cb(null, lines);
+		});
+	});
 };
 
 
 //with promise
 readFileAsArray(process.argv[2])
-.then(lines => {
-const numbers = lines.map(Number);
-const oddNumbers = numbers.filter(number => number % 2 === 1);
-console.log('odd numbers count:', oddNumbers.length);
-})
-.catch(console.error);
+	.then(lines => {
+		const numbers = lines.map(Number);
+		const oddNumbers = numbers.filter(number => number % 2 === 1);
+		console.log('odd numbers count:', oddNumbers.length);
+	})
+	.catch(console.error);
 
 
 //with callback
 readFileAsArray(process.argv[2], (err, lines) => {
 
-if(err) throw err;
+	if (err) throw err;
 
-const numbers = lines.map(Number);
-const oddNumbers = numbers.filter(number => number % 2 ===1);
-console.log('odd numbers count: ', oddNumbers.length);
+	const numbers = lines.map(Number);
+	const oddNumbers = numbers.filter(number => number % 2 === 1);
+	console.log('odd numbers count: ', oddNumbers.length);
 });
+
+//with async/await
+async function countOdd () {
+	try{
+		const lines = await readFileAsArray(process.argv[2]);
+		const numbers = lines.map(Number);
+		const oddNumbers = numbers.filter(number => number % 2 === 1);
+		console.log('odd numbers count:', oddNumbers.length);
+	}
+	catch(err){
+		console.error(err);
+	}
+}
+
+countOdd();
