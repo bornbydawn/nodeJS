1ab6262fc09741d1159413fc445af0663ee974cb
Ubuntu <ubuntu@ip-172-31-26-124.us-west-2.compute.internal>
2018-04-23 16:26:46 +0000

    server.js unformatted

--- networking/server.js
+++ networking/server.js
@@ -6,6 +6,26 @@ server.on('connection',socket =>{
 
     console.log('Client connected');
     socket.write('Welcome new client!\n');
+
+	socket.on('data', data => {
+	
+	console.log('data is:', data);	
+	socket.write('data is: ');
+
+	//output encoding as the second parameter as node does not take any defa	//ult encoding
+		
+	socket.write(data, 'utf8');
+
+	//socket.write(data)
+	});
+	//set default encoding as utf8, now stream is converted to string
+	//socket.setEncoding('utf8');
+
+
+	socket.on('end', () => {
+
+	console.log('Client disconnected');
+	});
 });
 
 server.listen(8000, ()=> console.log('Server bound'));
